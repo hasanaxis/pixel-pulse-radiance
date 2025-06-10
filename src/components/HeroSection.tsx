7b1520656e6b62f30377d28bbb5897a1e9b3375d
hasanaxis <hasan@axisimaging.com.au>
2025-06-10 15:39:05 +1000

    Changed images to webp

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -190,14 +190,14 @@ const HeroSection = () => {
                   <div className="space-y-2 sm:space-y-4 flex flex-col h-full lg:justify-center">
                     <div className="flex-1 lg:h-[40%] rounded-lg sm:rounded-2xl overflow-hidden shadow-lg">
                       <img
-                        src="/lovable-uploads/5002d0a4-7947-4e58-a0af-bbe2ae2e5de1.png"
+                        src="public/lovable-uploads/CT-laying.webp"
                         alt="Medical professional with patient"
                         className="w-full h-full object-cover"
                       />
                     </div>
                     <div className="flex-1 lg:h-[40%] rounded-lg sm:rounded-2xl overflow-hidden shadow-lg">
                       <img
-                        src="/lovable-uploads/5d65367b-6f40-4eee-8a33-fdd8d36fa22c.png"
+                        src="public/lovable-uploads/Xray-standing.webp"
                         alt="Medical imaging procedure"
                         className="w-full h-full object-cover"
                       />
@@ -208,7 +208,7 @@ const HeroSection = () => {
                   <div className="flex items-stretch lg:items-center lg:justify-center h-full">
                     <div className="w-full lg:h-full rounded-lg sm:rounded-2xl overflow-hidden shadow-lg">
                       <img
-                        src="/lovable-uploads/05c74ef0-abd6-40fc-9f5c-ec3cdf5b0302.png"
+                        src="public/lovable-uploads/CT-sitting.webp"
                         alt="Advanced medical imaging technology"
                         className="w-full h-full object-cover"
                       />
